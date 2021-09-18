9e50709f1df83c496e299b409d72e1a2bb44a962
adamdev <adamelfsborg.dev@gmail.com>
2021-09-18 13:25:26 +0200

    core done

--- instagramhelper/instagramhelper/settings.py
+++ instagramhelper/instagramhelper/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core.apps.CoreConfig',
+    'helper.apps.HelperConfig'
 ]
 
 MIDDLEWARE = [
